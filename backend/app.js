55f743544083130e5d4d64f3eaa637e33082b81e
jamie <jamietanamor049@gmail.com>
2025-12-13 00:03:29 +0800

    Added Functionality for Device to set itself Online

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import { dbConnection } from './config/db_access.js';
 import { config as dotenvConfig } from 'dotenv';
 import fs from 'fs';
 import deviceRouter from './routers/device.router.js';
+import checkOfflineDevices from './functions/checkOfflineDevices.js';
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
@@ -17,6 +18,10 @@ app.use("/device", deviceRouter);
 app.get("/", (req, res) =>{
     res.json({message: "Server is working!"})
 })
+
+setInterval(checkOfflineDevices, 30000);
+
+
 
 app.listen(PORT, ()=>{
     dbConnection();
